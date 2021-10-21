32846b4d122cfc15b00cc726e66ed6a431567ac7
Magnus9 <mstieglitz@protonmail.com>
2021-10-22 01:50:32 +0200

    add a few tests, mostly for API

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,27 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Coin from '@/views/Coin.vue'
+import API from '@/api/API'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('API', () => {
+  it('gets a 200 success response', async () => {
+    const resp = await API.getTopCryptoCurrencies()
+    expect(resp.status.code).toEqual(200)
+  })
+  it('has Data attribute in response', async () => {
+    const resp = await API.getTopCryptoCurrencies()
+    expect(resp.data.Data).toBeDefined()
+  })
+  it('has data content', async () => {
+    const resp = await API.getTopCryptoCurrencies()
+    expect(resp.data.Data.length).toBeGreaterThan(0)
+  })
+})
+
+describe('Coin.vue', () => {
+  it('should have the correct id', () => {
+    const wrapper = shallowMount(Coin, {
+      propsData: { id: 1024 }
     })
-    expect(wrapper.text()).toMatch(msg)
+    expect(wrapper.props().id).toEqual(1024)
   })
 })
